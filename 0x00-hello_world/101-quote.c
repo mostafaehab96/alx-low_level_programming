627126f78017372797909a79f8ee5d783a2f2a5e
mostafaehab96 <mostafa96ehab@gmail.com>
2022-12-08 12:27:44 -0800

    Modify task 8 for betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char *text = " - Dora Korpar, 2015-10-19\n"
+	char *text = " - Dora Korpar, 2015-10-19\n";
 	fprintf(stderr, "and that piece of art is useful\"%s", text);
 
 	return (1);
