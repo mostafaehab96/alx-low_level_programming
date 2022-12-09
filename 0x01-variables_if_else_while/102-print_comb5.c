26118667fd0e96471f24343c85e6ef9673e5ac94
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-10 00:26:28 +0200

    Add last task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int f1, f2, s1, s1;
+
+	f1 = 0;
+	while (f1 < 10)
+	{
+		f2 = 0;
+		while (f2 < 10)
+		{
+			s1 = f1;
+			while (s1 < 10)
+			{
+				s2 = f2 + 1;
+				while (s2 < 10)
+				{
+					putchar(f1 + '0');
+					putchar(f2 + '0');
+					putchar(' ');
+					putchar(s1 + '0');
+					putchar(s2 + '0');
+					if (f1 == 9 && f2 == 8 && s1 == 9 && s2 == 9)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+					s2++;
+				}
+				s1++;
+			}
+			f2++;
+		}
+		f1++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
