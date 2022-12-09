066d211992af939d940ee7e837c5ecfa1bcc0324
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-09 19:30:55 +0200

    Solve some logic error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,13 +18,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
-	printf("Last digit of %i is %i", n, last_digit);
+	printf("Last digit of %i is %i ", n, last_digit);
 	
 	if (last_digit > 5)
 	{
 		printf("and is greater than 5\n");
 	}
-	else if (last_digit < 6 && last_digit > 0)
+	else if (last_digit < 6 && last_digit != 0)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
