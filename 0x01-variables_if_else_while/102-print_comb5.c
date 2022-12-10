40bf4d88ed4b7a998e9f0aacaa0ddd74c353c6b9
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-10 13:55:30 +0200

    Resolve last task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,13 @@ int main(void)
 	int f1, f2, s1, s2;
 
 	f1 = 0;
+	f2 = 0;
+	s1 = 0;
 	s2 = 1;
 	while (f1 < 10)
 	{
-		f2 = 0;
 		while (f2 < 10)
 		{
-			s1 = f1;
 			while (s1 < 10)
 			{
 				while (s2 < 10)
@@ -27,12 +27,11 @@ int main(void)
 					putchar(' ');
 					putchar(s1 + '0');
 					putchar(s2 + '0');
-					if (f1 == 9 && f2 == 8 && s1 == 9 && s2 == 9)
+					if (!(f1 == 9 && f2 == 8 && s1 == 9 && s2 == 9))
 					{
-						break;
+						putchar(',');
+						putchar(' ');
 					}
-					putchar(',');
-					putchar(' ');
 					s2++;
 				}
 				s1++;
@@ -40,12 +39,13 @@ int main(void)
 			}
 			f2++;
 			s2 = f2 + 1;
+			s1 = f1;
 		}
 		f1++;
+		f2 = 0;
+		s1 = f1;
+		s2 = f2 + 1;
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
