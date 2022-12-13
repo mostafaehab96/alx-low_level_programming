9591a6ac9ffae6cf0310de9366bb3aa5378a285f
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-14 00:54:40 +0200

    Add task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: the number to start printing from
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%i, ", i);
+		}
+	}
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%i, ", i);
+	}
+}
