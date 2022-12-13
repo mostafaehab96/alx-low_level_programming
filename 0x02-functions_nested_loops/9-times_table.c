9c070e079be6daca28d69b2da48c023ad3b14b8e
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-14 00:41:01 +0200

    Add task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * print_digits - prints all digits of number n
+ * @n: the number that will be printed
+ */
+
+void print_digits(int n)
+{
+	int tmp = n;
+	int digits = 0;
+	int zero_count = 0;
+
+	while (tmp > 0)
+	{
+		digits = digits * 10 + (tmp % 10);
+		if (tmp % 10 == 0)
+			zero_count++;
+		tmp /= 10;
+	}
+	do {
+		_putchar(digits % 10 + '0');
+		digits /= 10;
+	} while (digits > 0);
+
+	for (int i = 0; i < zero_count; i++)
+	{
+		_putchar('0');
+	}
+
+
+}
+
+/**
+ * times_table - prints times table from 0 to 9
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			print_digits(i * j);
+			if (j == 9)
+			{
+				_putchar('$');
+				break;
+			}
+			_putchar(',');
+			_putchar(' ');
+			if ((i * (j + 1)) < 10)
+				_putchar(' ');
+
+		}
+		_putchar('\n');
+	}
+
+}
