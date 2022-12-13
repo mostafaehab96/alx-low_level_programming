af2c93f77514950931f48d34f4ea1bf4452bf562
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-13 16:52:06 +0200

    Add task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabetical letters in lower case
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+}
