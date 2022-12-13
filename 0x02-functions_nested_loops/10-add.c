2c4ccf7e0dcdb0e93817c4ddd73e78654f34d522
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-14 00:47:04 +0200

    Add task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adds two integers and returns the result
+ * @x: first integer to add
+ * @y: second integer to add
+ *
+ * Return: the result of added integer
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
