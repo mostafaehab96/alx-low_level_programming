06a8a0ad83808c4989f91e439b710d45b4bf813e
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-13 23:42:34 +0200

    Add task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_two_digits - print any number that is no more than
+ * two digits in 00 format
+ * @n: the number to print it's digits
+ */
+
+
+void print_two_digits(int n)
+{
+	if (n < 10)
+	{
+		_putchar('0');
+		_putchar(n + '0');
+	}
+	else
+	{
+		int last_digit = n % 10;
+		int first_digit = (n - last_digit) / 10;
+
+		_putchar(firt_digit + '0');
+		_putchar(last_digit + '0');
+	}
+}
+
+/**
+ * jack_bauer - prints all hours and minutes in a day
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			print_two_digits(i);
+			_putchar(':');
+			print_two_digits(j);
+			_putchar('\n');
+		}
+	}
+
+}
