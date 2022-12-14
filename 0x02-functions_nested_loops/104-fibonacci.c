f4e8ac9a72a392904c8c2013ef1236102f2a1927
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-14 21:34:02 +0200

    Resolve the task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,22 +9,28 @@
 int main(void)
 {
 	int i;
-	unsigned long int n;
-	unsigned long int first = 1;
-	unsigned long int second = 2;
+	double n;
+	double first = 1;
+	double second = 2;
 
 	for (i = 0; i < 98; i++)
 	{
 		if (i == 0)
+		{
 			printf("1, ");
+			continue;
+		}
 		if (i == 1)
+		{
 			printf("2, ");
+			continue;
+		}
 
 		n = first + second;
 		first = second;
 		second = n;
 
-		printf("%lu", n);
+		printf("%.0lf", n);
 
 		if (i < 97)
 			printf(", ");
