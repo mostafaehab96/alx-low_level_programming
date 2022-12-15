011252686ec4d362ebb78a2b27f6a21ca9077067
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-16 00:29:25 +0200

    Add task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal of length n
+ * @n: length of the diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int s;
+
+	for (i = 0; i < n; i++)
+	{
+		for (s = 0; s < i + 1; s++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
