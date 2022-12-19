03b45c3d000cebc83db4b42eb03050756d5657ed
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-19 14:03:10 +0200

    Add task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: string we need to reverse
+ */
+
+void rev_string(char *s)
+{
+	int length = strlen(s);
+	char copy[length];
+	int i;
+	int j = 0;
+
+	strcpy(copy, s);
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		s[i] = copy[j];
+		j++;
+	}
+}
