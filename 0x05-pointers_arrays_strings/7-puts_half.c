c5d27672708fad27cc966c207acd7074ec7a8878
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-19 16:16:57 +0200

    add task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of the string
+ * @str: string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int half;
+	int i;
+
+	if (length % 2 == 0)
+		half = length / 2;
+
+	half = (length - 1) / 2;
+
+	for (i = half; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+
+}
