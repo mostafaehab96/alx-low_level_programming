bfe2dd2bf3958dbabbf2bd413556c30edf25465c
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-19 13:51:29 +0200

    Stupid errors

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,9 @@ void print_rev(char *s)
 	int length = strlen(s);
 	int i;
 
-	for (i = lenght - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 	{
-		_putchar(s[i])
+		_putchar(s[i]);
 	}
 
 	_putchar('\n');
