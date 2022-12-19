29cb9d49034d2f5b648e150821a187095b3ca052
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-19 13:36:30 +0200

    Add task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#inlude "main.h"
+
+/**
+ * print_rev - prints a string in a reverse order
+ * @s: the string to be printed
+ */
+
+void print_rev(char *s)
+{
+	if (s[0] == '\0')
+		return;
+
+	print_rev(++s);
+	_putchar(s[0]);
+}
+
