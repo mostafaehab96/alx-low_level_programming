c07ee1688f96e824350b246c235e31af76c9da64
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-19 12:49:14 +0200

    Add project readme, main.h and first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a number and convert it to 98
+ * @n: number that will be converted
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme for project 0x05-pointers_arrays_strings
