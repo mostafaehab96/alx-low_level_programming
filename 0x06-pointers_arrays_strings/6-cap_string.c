57274fba1bc7ac2e36359430e234ca770767638b
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-22 22:12:00 +0200

    Add task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,68 @@
+#include <ctype.h>
+#include "main.h"
+
+#define True 1
+#define False 0
+typedef int bool;
+
+/**
+ * check_existance - check if charcter c exist in string s
+ * @c: character to search for
+ * @s: string to search in
+ * Return: true if it's exitst false otherwise
+ */
+
+bool check_existance(char c, char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (c == s[i])
+			return (True);
+		i++;
+	}
+
+	return (False);
+}
+
+/**
+ * is_splitter - checks if character c is a splitter in a sentence
+ * @c: character to check
+ * Return: true if it is, false otherwise
+ */
+bool is_splitter(char c)
+{
+	return (check_existance(c, " \n\t,.;!?\"(){}"));
+}
+
+/**
+ * cap_string - capitalize a given string s
+ * @s: string to be capitalized
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	bool new_word = True;
+
+	while (s[i] != '\0')
+	{
+		if (isalnum(s[i]))
+		{
+			if (new_word)
+			{
+				s[i] = toupper(s[i]);
+				new_word = False;
+			}
+		}
+		else if (is_splitter(s[i]))
+		{
+			new_word = True;
+		}
+		i++;
+	}
+
+	return (s);
+}
