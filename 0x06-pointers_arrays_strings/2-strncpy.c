7599c8f6b08bdd862bc4f2863e68d4f34c363cb0
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2022-12-22 17:57:01 +0200

    Fix error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,12 +16,13 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (src[i] == '\0')
+		{
+			dest[i] = '\0';
 			break;
+		}
 
 		dest[i] = src[i];
 	}
-
-	dest[i] = '\0';
 
 	return (dest);
 }
