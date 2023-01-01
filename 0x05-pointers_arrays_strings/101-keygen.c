384ed22350cd7fe42642bcef452b0244c08e88dc
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-01 23:49:50 +0200

    Try to resolve last task again

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,12 +12,13 @@ int main(void)
 	long t = time(NULL);
 	int r;
 	int i;
+	char printable_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
 
 	srand((unsigned int) t);
 	for (i = 0; i < 9; i++)
 	{
-		r = rand() % 10;
-		printf("%i", r % 10);
+		r = rand() % (sizeof(printable_chars) - 1);
+		printf("%c", printable_chars[r]);
 	}
 
 	return (0);
