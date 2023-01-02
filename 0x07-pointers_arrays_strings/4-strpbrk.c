3154aaf0fc87331442a38cbde42a0630e58cb723
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-02 19:31:09 +0200

    Fix error

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,27 @@
 #include "main.h"
 #include <string.h>
 
+
+/**
+ * strcontain - function to check if character c is in string s
+ * @s: the string to be scanned
+ * @c: the character to be checked for
+ * Return: 1 if exists, 0 otherwise
+ */
+
+int strcontain(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (1);
+	}
+
+	return (0);
+}
+
 /**
  * _strpbrk - a function locates the first occurrence in the
  * string s of any of the bytes in the string accept
@@ -15,11 +36,11 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (strchr(accept, s[i]) != NULL)
+		if (strcontain(accept, s[i]) != 0)
 			return (&s[i]);
 	}
 
-	if (strchr(accept, s[i]) != NULL)
+	if (strcontain(accept, s[i]) != 0)
 		return (&s[i]);
 
 	return (NULL);
