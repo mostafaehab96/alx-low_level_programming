6c0d91ed29f8d359608f813d92926188b3753fad
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-03 19:46:12 +0200

    Fix errors

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 /**
  * _strstr - searches in haystack for the first occcurence of needle
@@ -16,6 +17,7 @@ char *_strstr(char *haystack, char *needle)
 	int found = 0;
 	int ndl_len = strlen(needle);
 	int hay_len = strlen(haystack);
+	int ctr;
 
 	if (hay_len < ndl_len)
 		return (NULL);
@@ -27,19 +29,20 @@ char *_strstr(char *haystack, char *needle)
 	{
 		if (haystack[i] == needle[0])
 		{
+			ctr = 0;
 			found = 1;
-			for (j = i + 1; j < ndl_len; j++)
+			for (j = i; j < i + ndl_len; j++)
 			{
-				if (haystack[j] != needle[j])
+				if (haystack[j] != needle[ctr])
 				{
 					found = 0;
 					break;
 				}
+				ctr++;
 			}
 
 			if (found)
 				return (&haystack[i]);
-			i += ndl_len;
 		}
 		i++;
 	}
