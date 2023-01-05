68a5dd391b13002093db7a4600f9e5c4212ec2bd
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-06 00:41:36 +0200

    Add some comments

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -21,9 +21,15 @@ int wildcmp(char *s1, char *s2)
 	if (s2[0] == '*' && s2[1] != '\0' && s1[0] == '\0')
 		return (0);
 
+	/* if the first characters identical compare the next */
 	if (s1[0] == s2[0])
 		return (wildcmp(s1 + 1, s2 + 1));
 
+	/**
+	 * if there is '*', then we consider the current
+	 * character is the first string or we ignore
+	 * the current character is the first string
+	 */
 	if (s2[0] == '*')
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
