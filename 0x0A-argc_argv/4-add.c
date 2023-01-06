6c12187315bd7241e3cec8dac371cb0d3e315ec2
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-06 22:57:47 +0200

    Fix error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_all_digit - checks that every character in a given string is digit
+ * @s: the string to be checked
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+
+int is_all_digit(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
+			return (0);
+	}
+
+	return (1);
+}
 
 /**
  * main - adds all integers that passed as arguments and prints them
@@ -17,7 +37,7 @@ int main(int argc, char **argv)
 	for (i = 1; i < argc; i++)
 	{
 		n = atoi(argv[i]);
-		if (n == 0 && argv[i][0] != '0')
+		if (n == 0 && !is_all_digit(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
