55def4f5ca0d951be6c79cea8b739dd25145b491
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-06 22:13:18 +0200

    Add main.h and second task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints argument count
+ * @argc: arguments count
+ * @argv: the arguemnts
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%i\n", argc);
+	return (0);
+}
