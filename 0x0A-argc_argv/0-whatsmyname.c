9b708943df540c3827746811fb163c45b0684dcd
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-06 22:06:34 +0200

    Add first task and project requirements

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints program name
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
