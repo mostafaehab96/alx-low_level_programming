145561f6da22c76953bd08ea9455c7d2637ea4c7
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-12 23:33:53 +0200

    Add task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocates a memory in the heap and intialize it to 0
+ * @nmemb: the number of elements to be allocated
+ * @size: the size of each element
+ * Return: an array of memory allocated and set to 0, or NULL if failed
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *memory = malloc(size * nmemb);
+
+	if (size == 0 || memory == NULL)
+		return (NULL);
+
+	memory = memset(memory, 0, nmemb * size);
+
+	return (memory);
+}
