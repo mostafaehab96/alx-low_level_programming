c6578b032db96846858622f1c0442a8516fa5e05
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-13 00:17:29 +0200

    Fixing error in task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * array_range - create an array of integers from min to max
  * @min: the minimun value in array
@@ -12,9 +12,12 @@ int *array_range(int min, int max)
 {
 	int i;
 	int size = max - min + 1;
-	int *arr = malloc(sizeof(int) * size);
+	int *arr;
 
-	if (min > max || arr == NULL)
+	if (min > max)
+		return (NULL);
+	arr = malloc(sizeof(int) * size);
+	if (arr == NULL)
 		return (NULL);
 
 	for (i = min; i <= max; i++)
