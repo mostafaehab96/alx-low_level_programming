513f0704c717f577a44e8bd224c675804dc161b0
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-16 20:23:28 +0200

    Add task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,24 +12,24 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *dog_name = NULL;
-	char *owner_name = NULL;
-	dog_t *my_dog = malloc(sizeof(dog_t));
+	char *dog_name;
+	char *owner_name;
+	dog_t *my_dog;
 
-	if (my_dog == NULL || name == NULL || owner == NULL)
+	if (name == NULL || owner == NULL || age < 0)
 		return (NULL);
-	if (name == NULL)
-		my_dog->name = NULL;
-	else
-		dog_name = malloc(sizeof(char) * strlen(name) + 1);
-		dog_name = strcpy(dog_name, name);
-		my_dog->name = dog_name;
-	if (owner == NULL)
-		my_dog->owner = NULL;
-	else
-		owner_name = malloc(sizeof(char) * strlen(owner) + 1);
-		owner_name = strcpy(owner_name, owner);
-		my_dog->owner = owner_name;
+
+	my_dog = malloc(sizeof(dog_t));
+
+	if (my_dog == NULL)
+		return (NULL);
+
+	dog_name = malloc(sizeof(char) * strlen(name) + 1);
+	dog_name = strcpy(dog_name, name);
+	my_dog->name = dog_name;
+	owner_name = malloc(sizeof(char) * strlen(owner) + 1);
+	owner_name = strcpy(owner_name, owner);
+	my_dog->owner = owner_name;
 	my_dog->age = age;
 
 	return (my_dog);
