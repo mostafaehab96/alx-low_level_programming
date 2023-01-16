0530059467d654e10a861a98c45bf91a3392084a
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-16 16:48:57 +0200

    Add first task and project requirements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
