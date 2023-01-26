18b6058fe925692ef7987bc551dfdf560b561910
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-26 22:43:41 +0200

    Add task 4 free the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - deletes all nodes in a list
+ * @head: the head of list to be deleted
+ */
+void free_list(list_t *head)
+{
+	list_t *p = head;
+
+	while (head != NULL)
+	{
+		p = head;
+		head = head->next;
+		free(p);
+	}
+}
