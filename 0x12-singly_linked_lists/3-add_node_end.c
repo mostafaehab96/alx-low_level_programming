17db85d8f1a51bf6b3fd4068508cb7dd5ac4df1a
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-26 22:34:58 +0200

    Trying to remove strlen

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * add_node_end - append node at the end of a list
@@ -12,6 +12,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	char *s;
+	int len;
 	list_t *node = malloc(sizeof(list_t));
 	list_t *last = *head;
 
@@ -24,9 +25,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(node);
 		return (NULL);
 	}
-
+	for (len = 0; s[len] != '\0';)
+		len++;
 	node->str = s;
-	node->len = strlen(s);
+	node->len = len;
 	node->next = NULL;
 
 	if (last == NULL)
