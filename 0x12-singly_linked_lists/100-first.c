e895d4663f2ea65a7ede54bfe9a53f8e0d3aab92
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-26 23:01:07 +0200

    Add first advanced task print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_first - prints a sentence before the main function is executed
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,"
+			"\nI bore my house upon my back!\n");
+}
