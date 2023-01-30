c1745552a9c63c4e3be172c39c4fe9a069d17732
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-30 23:28:31 +0200

    Return to previou solution with fixing error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,9 +15,9 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		p = (*head)->next;
-		free(*head);
-		*head = p;
+		p = *head;
+		*head = (*head)->next;
+		free(p);
 	}
 	head = NULL;
 }
