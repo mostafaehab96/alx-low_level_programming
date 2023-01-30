47f8f9403bd41a7b6fcb3baafd52a789917b1298
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-01-30 23:15:52 +0200

    Add task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - fress a list
+ * @head: a pointer to the head of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *p = NULL;
+
+	while (*head != NULL)
+	{
+		p = *head;
+		*head = (*head)->next;
+		free(p);
+	}
+}
+
