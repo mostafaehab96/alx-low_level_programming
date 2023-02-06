e3927f2c5f40f79e1cb21f39a31d0b77cf510be1
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-02-06 23:40:44 +0200

    Add first task, readme file, header file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file with system calls
+ * @filename: the name of the file to be red
+ * @letters: the number of letters we want to read
+ * Return: the number of letters that was red successfully
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buffer[1];
+	ssize_t count = 0;
+	size_t i;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return (0);
+
+
+	for (i = 0; i < letters; i++)
+	{
+		if (read(fd, buffer, 1))
+			write(STDOUT_FILENO, buffer, 1);
+		else
+			return (count);
+
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+## readme for the project 0x15-file_io
