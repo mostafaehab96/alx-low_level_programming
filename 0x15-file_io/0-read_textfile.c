717766a0f641a4190f01726267e3ee36ccf4f454
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-02-06 23:56:56 +0200

    Try another solution

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,30 +9,32 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buffer[1];
-	ssize_t count = 0;
-	size_t i;
+	char *buffer = malloc(sizeof(char) * letters);
+	ssize_t red;
+	ssize_t written;
+
+	if (buffer == NULL)
+		return (0);
 
 	if (filename == NULL)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	fd = open(filename, O_RDONLY);
+	red = read(fd, buffer, letters);
+	written = write(STDOUT_FILENO, buffer, red);
 
-	if (fd < 0)
-		return (0);
-
-
-	for (i = 0; i < letters; i++)
+	if (fd < 0 || red < 0 || written < 0)
 	{
-		if (read(fd, buffer, 1))
-			write(STDOUT_FILENO, buffer, 1);
-		else
-			return (count);
-
-		count++;
+		free(buffer);
+		return (0);
 	}
+
 
 	close(fd);
 
-	return (count);
+
+	return (written);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
