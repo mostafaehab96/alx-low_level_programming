135ee03cfd7abfcdb651010e82f21719e7d9665f
mostafaehab96 <mostafa96ehab@gmail.com>
2023-04-07 15:32:07 +0000

    Add betty docs and one condition to check if value is empty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,12 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: is the key. key can not be an empty string
+ * @value: value is the value associated with the key
+ * Return: 0 if success 1 if fail
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
@@ -8,6 +14,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t **arr;
 
 	if (!ht || !value)
+		return (1);
+
+	if (strlen(value) == 0)
 		return (1);
 
 	new = malloc(sizeof(hash_node_t));
