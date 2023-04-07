ae0a01bb90bb8275c206be0c808fc03fc646bc13
mostafaehab96 <mostafa96ehab@gmail.com>
2023-04-07 16:41:36 +0000

    Check if the key is NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t **arr;
 
-	if (!ht || !value)
+	if (!ht || !value || !key)
 		return (0);
 
 	new = malloc(sizeof(hash_node_t));
