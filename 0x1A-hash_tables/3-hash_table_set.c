db453188e876fcf5aab4583280b3b22e25f12b06
mostafaehab96 <mostafa96ehab@gmail.com>
2023-04-07 16:49:52 +0000

    Using strdup with key and check it's length

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,13 +13,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t **arr;
 
-	if (!ht || !value || !key)
+	if (!ht || !value || !key || strlen(key) == 0)
 		return (0);
 
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-	new->key = (char *)key;
+	new->key = strdup(key);
 	new->value = strdup(value);
 	index = key_index((const unsigned char *)key, ht->size);
 	arr = ht->array;
