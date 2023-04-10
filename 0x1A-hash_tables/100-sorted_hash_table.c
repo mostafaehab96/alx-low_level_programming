d82e8c3ae4b4a894fbf425cd5a74f23982aaf492
mostafaehab96 <mostafa96ehab@gmail.com>
2023-04-10 11:53:15 +0000

    Add shash_table_get function

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -229,3 +229,28 @@ void shash_table_delete(shash_table_t *ht)
 	free(ht->array);
 	free(ht);
 }
+
+/**
+ * shash_table_get - gets the value of a key in hashtable
+ * @ht: the hashtable to search into
+ * @key: the key to search for
+ * Return: the value associated with the element
+ * or NULL if key couldn't be found
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	shash_node_t *tmp = NULL;
+	unsigned long int index;
+
+	if (!ht || !key || strlen(key) == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *) key, ht->size);
+	tmp = ht->array[index];
+	while (tmp && strcmp(key, tmp->key))
+		tmp = tmp->next;
+
+	if (tmp)
+		return (tmp->value);
+	return (NULL);
+}
