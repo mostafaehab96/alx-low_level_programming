4c2fe24c97d7382b61f3743abe422150d730cb2b
mostafaehab96 <mostafa96ehab@gmail.com>
2023-07-10 16:57:47 +0000

    Add project 0x1E-search_algorithms, README, header file search_algos.h and first task 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search for an element in an array in linear algorithm
+ * @array: the array to look at
+ * @size: the size of the array
+ * @value: the value to look for
+ * Return: the index of the value if being found, -1 if not
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
